dfb71ab3845690dc6223450ccd5f76650ebcc574
Hafizu <piz230601@gmail.com>
2024-02-19 11:40:53 +0800

    initial commit

---
+++ src/components/typography.tsx
@@ -0,0 +1,31 @@
+export const H1: React.FC<{ text: string }> = ({ text }) => {
+	return (
+		<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+			{text}
+		</h1>
+	);
+};
+
+export const H2: React.FC<{ text: string }> = ({ text }) => {
+	return (
+		<h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+			{text}
+		</h2>
+	);
+};
+
+export const H3: React.FC<{ text: string }> = ({ text }) => {
+	return (
+		<h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">{text}</h3>
+	);
+};
+
+export const H4: React.FC<{ text: string }> = ({ text }) => {
+	return (
+		<h4 className="scroll-m-20 text-xl font-semibold tracking-tight">{text}</h4>
+	);
+};
+
+export const Parag: React.FC<{ text: string }> = ({ text }) => {
+	return <p className="leading-7 [&:not(:first-child)]:mt-6">{text}</p>;
+};
