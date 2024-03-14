ff1893d0ed290ed8160d89667541bbde6aab381b
Hafizu <piz230601@gmail.com>
2024-03-14 13:39:28 +0800

    add ignore

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,5 @@
 import type { NextAuthConfig } from "next-auth";
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/GitHub";
 import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./db";
