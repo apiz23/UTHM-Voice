446716a972af9132b51d58050cfbc24b812b9986
Hafizu <piz230601@gmail.com>
2024-05-06 00:27:47 +0800

    test auth

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,28 +1,19 @@
 "use client";
 
 import {
-	CardContent,
 	CardFooter,
 	Card,
 	CardDescription,
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
-import { useState } from "react";
-import { Input } from "@/components/ui/input";
-import GithubSignInButton from "@/components/GithubSignInButton";
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Github } from "lucide-react";
 
 export default function Page() {
 	const { data: session, status } = useSession();
-
-	const [data, setData] = useState({ email: "", password: "" });
-
-	const handleChange = (e: any) => {
-		const { name, value } = e.target;
-		setData((prev) => ({ ...prev, [name]: value }));
-	};
 
 	if (!session) {
 		return (
@@ -34,31 +25,16 @@ export default function Page() {
 							<CardDescription>Admin Only</CardDescription>
 						</CardHeader>
 
-						<form>
-							<CardContent>
-								<div className="grid w-full items-center gap-4">
-									<div className="flex flex-col space-y-1.5">
-										<Input
-											type="text"
-											name="email"
-											placeholder="Email"
-											value={data.email}
-											onChange={handleChange}
-											autoComplete="1"
-										/>
-										<Input
-											type="password"
-											name="password"
-											placeholder="Password"
-											value={data.password}
-											onChange={handleChange}
-										/>
-									</div>
-								</div>
-							</CardContent>
-						</form>
 						<CardFooter className="flex justify-center">
-							<GithubSignInButton />
+							<Button
+								onClick={() =>
+									signIn("github", {
+										callbackUrl: `${window.location.origin}/admin/dashboard`,
+									})
+								}
+							>
+								<Github />
+							</Button>
 						</CardFooter>
 					</Card>
 				</div>
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,44 +3,15 @@ import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./db";
-import GitHub from "next-auth/providers/github";
 
 export const authOptions = {
 	adapter: PrismaAdapter(prisma),
-	// providers: [GitHub],
 	providers: [
 		GitHubProvider({
 			clientId: process.env.AUTH_GITHUB_ID as string,
 			clientSecret: process.env.AUTH_GITHUB_SECRET as string,
 		}),
 	],
-	session: {
-		strategy: "jwt",
-	},
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signOut } = NextAuth(authOptions);
-// import NextAuth from "next-auth";
-// import GitHub from "next-auth/providers/github";
-// import type { NextAuthConfig } from "next-auth";
-
-// export const config = {
-// 	theme: {
-// 		logo: "https://next-auth.js.org/img/logo/logo-sm.png",
-// 	},
-// 	providers: [GitHub],
-// 	basePath: "/admin/dashboard",
-// 	callbacks: {
-// 		authorized({ request, auth }) {
-// 			const { pathname } = request.nextUrl;
-// 			if (pathname === "/middleware-example") return !!auth;
-// 			return true;
-// 		},
-// 		jwt({ token, trigger, session }) {
-// 			if (trigger === "update") token.name = session.user.name;
-// 			return token;
-// 		},
-// 	},
-// } satisfies NextAuthConfig;
-
-// export const { handlers, auth, signIn, signOut } = NextAuth(config);
